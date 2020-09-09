283ee57eefe76de222c19a8b79535856334a4cd1
Kiyotaka <t10kiyotaka@gmail.com>
2020-09-10 08:51:45 +0900

    Add package for generate English words

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Text(nouns.first),
+          ),
+        ),
+      ),
+    );
+  }
 }
