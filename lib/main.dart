5d50c9b08b713314a33fde770c2d568d7178dcd7
Kiyotaka <t10kiyotaka@gmail.com>
2020-09-11 07:24:30 +0900

    Add 7 colors sound bars

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,34 @@ void main() {
 class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    int init = 1;
+    final List<MaterialColor> colors = [
+      Colors.red,
+      Colors.orange,
+      Colors.yellow,
+      Colors.green,
+      Colors.teal,
+      Colors.blue,
+      Colors.purple,
+    ];
+    void playSound(int soundNumber) {
+      final player = AudioCache();
+      player.play('note$soundNumber.wav');
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note2.wav');
-                },
-                child: Text('Click Me')),
+          child: Column(
+            children: <Widget>[
+              for (var i = init; i <= 7; i++)
+                FlatButton(
+                  onPressed: () {
+                    playSound(i);
+                  },
+                  color: colors[i - 1],
+                ),
+            ],
           ),
         ),
       ),
