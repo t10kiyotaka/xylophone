46634ecf76442296579433062f161882553471c2
Kiyotaka <t10kiyotaka@gmail.com>
2020-09-11 08:33:25 +0900

    Refactor: Extract Expand widget to function

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,30 @@ class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     int init = 1;
-    final List<MaterialColor> colors = [
-      Colors.red,
-      Colors.orange,
-      Colors.yellow,
-      Colors.green,
-      Colors.teal,
-      Colors.blue,
-      Colors.purple,
-    ];
+
     void playSound(int soundNumber) {
       final player = AudioCache();
       player.play('note$soundNumber.wav');
+    }
+
+    Expanded buildKey(int i) {
+      final List<MaterialColor> colors = [
+        Colors.red,
+        Colors.orange,
+        Colors.yellow,
+        Colors.green,
+        Colors.teal,
+        Colors.blue,
+        Colors.purple,
+      ];
+      return Expanded(
+        child: FlatButton(
+          onPressed: () {
+            playSound(i);
+          },
+          color: colors[i - 1],
+        ),
+      );
     }
 
     return MaterialApp(
@@ -29,15 +41,7 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              for (var i = init; i <= 7; i++)
-                Expanded(
-                  child: FlatButton(
-                    onPressed: () {
-                      playSound(i);
-                    },
-                    color: colors[i - 1],
-                  ),
-                ),
+              for (var i = init; i <= 7; i++) buildKey(i),
             ],
           ),
         ),
